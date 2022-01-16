8286c21d8f34dc180d29f70838007ca74afd1384
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2022-01-16 19:22:14 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 
-YOU_API_KEY="AIzaSyCxFZ0FLBO7z_ENKLTtvNTDcC8BHwUsymA"
-query="https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=Westboro&type=video&key=${YOU_API_KEY}"
+YOU_API_KEY="Create your own API Key and paste here"
+query="https://youtube.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q=Westboro&maxResults=3&type=video&key=${YOU_API_KEY}"
 echo $query
 
 curl --request GET $query \
