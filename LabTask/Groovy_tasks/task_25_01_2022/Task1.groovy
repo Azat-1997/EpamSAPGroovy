899a1907244eefe5d85c4f7529407b03e5f9df62
Azat-1997 <54988916+Azat-1997@users.noreply.github.com>
2022-01-28 13:27:16 +0300

    Add files via upload

--- LabTask/Groovy_tasks/task_25_01_2022/Task1.groovy
+++ LabTask/Groovy_tasks/task_25_01_2022/Task1.groovy
@@ -1,61 +1,67 @@
 public class Task1 {
- 
-    static int[] insertionSort(int[] array) {
-      int tmp
-      int[] sortedArray = array.clone()
-
-      for (int i=1; i < sortedArray.length; i++) {
-  
-          for (int j=0; j < i; j++) {
-
-             if (sortedArray[j] > sortedArray[i]) {
-                 tmp = sortedArray[i]
-                 sortedArray[i] = sortedArray[j]
-                 sortedArray[j] = tmp
-               }    
-
-             }         
-         
-        }
-
-        return sortedArray
-
-                    }
 
     public static void main(String[] args) {
         println("task1")
         int[] array = [2, 1, 0, 9, 7, 9 , 2, 2, 10, 15, 26, -11] 
-        def closureSort = {println insertionSort(array)}
-        closureSort.call()
+        def closureSort = {arr -> 
+                           int tmp
+                           /* Work with left part and keep it sorted:
+                              when we add new element in left part we compare it with all elements in left segment
+                              and swap them if new element is smaller (sort by increasing).
+                           */
+                           for(int i=1; i < arr.length; i++) { 
+ 
+                                  for(int j=0; j < i; j++) {
+    
+                                       if (arr[i] < arr[j]) {
+                                             tmp = arr[i] 
+                                             arr[i] = arr[j]
+                                             arr[j] = tmp                                                   
+                                             
+                                            }  
+                                    }
+
+                                 }
+
+                            println("sorted array is ${arr}")
+                           
+                          }
+          println("source array is ${array}")
+          closureSort(array)
+
+
         println("task2")
         def anotherClosure = {number -> 
             println("${number * number}")
-            closureSort.call(array)
+            closureSort(array)
           }
-        anotherClosure.call(5)
+        anotherClosure(5)
         println("task3")
         def twoParamsClosure = {number, closure -> 
-                               closure.call(number)
+                               closure(number)
              }
 
-        twoParamsClosure.call(5, anotherClosure)
+        twoParamsClosure(5, anotherClosure)
          
         println("task4")
         def squareClosure = {x -> x * x}
         println("\n    For cycle")
         for (int element : array) {
 
-           println("       ${element} ^ 2 = ${squareClosure.call(element)}")
+           println("       ${element} ^ 2 = ${squareClosure(element)}")
 
            }
 
         println("\n     Each cycle")
 
-        array.each({it -> println("       ${it} ^ 2 = ${squareClosure.call(it)}") })
+        array.each({it -> println("       ${it} ^ 2 = ${squareClosure(it)}") })
 
      
 
        println("\ntask5")
+       /* if we want to compare by last digit we can use residuals of division by 10:
+          all possible residuals 0, 1, 2 .. 9 (or n-1 where n is denominator) 
+       */
        def compareLastDigits = {x, y -> 
                                       if(Math.abs(x) % 10 > Math.abs(y) % 10) {
                                               x  
@@ -64,8 +70,8 @@ public class Task1 {
                                          }
                                 }
       
-       println(compareLastDigits.call(20, -11)) // -11
-       println(compareLastDigits.call(23, 25)) // 25
+       println(compareLastDigits(20, -11)) // -11
+       println(compareLastDigits(23, 25)) // 25
  }
 
 }
