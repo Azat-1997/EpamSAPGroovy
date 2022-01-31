68f583641b578866b0fde548a1bee66907c80116
Azat-1997 <armingaleev97@yandex.ru>
2022-01-31 12:34:08 +0300

    Make five task with files

--- LabTask/Groovy_tasks/task_27_01_2022/FilesDemo.groovy
+++ LabTask/Groovy_tasks/task_27_01_2022/FilesDemo.groovy
@@ -3,9 +3,7 @@ public class FilesDemo {
   public static void createFile(String filename, int minimum, int maximum, int nrow) {
 
   Random generator = new Random()
-
   File testFile = new File(filename)
-
   // create a text
   def data = []
   for(int j=0; j<nrow; j++) {
@@ -20,6 +18,38 @@ public class FilesDemo {
 
   }
 
+  public static void mergeFiles(File[] files, int nrow, String output) {
+    File mergedFile = new File(output)
+    def iters = []
+    // create list of iterators
+    files.each {iters << it.iterator()}
+    int total
+    def row
+    // read each file step by step:
+    // take first line of each file and repeat procedure for further lines
+    
+    def title = []
+    files.each {title << it.name}
+    title << "total"
+
+
+    mergedFile.withWriter("utf-8") { writer ->
+    writer.writeLine title.join(',')
+    for (int i = 0; i < nrow; i++) {
+        total = 0
+        row = []
+        iters.each{
+                   String elem = it.next()
+                   row << elem
+                   total += Integer.valueOf(elem)
+                  }
+        row << (String) total
+        
+        writer.writeLine row.join(',')
+        }
+
+  }
+}
   public static void main(String[] args) {
 
     def generateFiles = {
@@ -35,13 +65,9 @@ public class FilesDemo {
 
     // read files
     File directory = new File("test")
-    // take list of files 
-    File[] files = directory.listFiles() 
-    def row = []
-    int total = 0
-    def matrix = []
-    files.each({matrix << it.readLines()})
-    
-      
+    File[] files = directory.listFiles()
+    int nrow = 1000
+    mergeFiles(files, nrow, "mergedFiles.csv")
+   
     }
 }
